94e59500dae970089d24cd9d2bcf1ea1bf0702b2
Adrian Mostowski <adaxelx@o2.pl>
2022-01-22 16:38:33 +0100

    randomCategory: add blocking when category is active

--- app/api/bookCategory.ts
+++ app/api/bookCategory.ts
@@ -8,9 +8,28 @@ type GetAllCategoriesBody = {
   bookGroupId: string | number;
 } & WithToken;
 
+type SendBookCategory = GetAllCategoriesBody & {
+  body: Partial<BookCategory>;
+  bookCategoryId?: string;
+};
+
 export const getCategories = ({
   bookGroupId,
   token,
 }: GetAllCategoriesBody): Promise<BookCategory[]> => {
   return client(`${url}/${bookGroupId}/bookCategory/all`, { token });
+};
+
+export const updateBookCategory = ({
+  bookGroupId,
+  token,
+  body,
+  bookCategoryId,
+}: SendBookCategory): Promise<BookCategory> => {
+  console.log(body);
+  return client(`${url}/${bookGroupId}/bookCategory/${bookCategoryId}`, {
+    token,
+    body,
+    method: 'PATCH',
+  });
 };
--- app/routes/book-group/$bookGroupId/index.tsx
+++ app/routes/book-group/$bookGroupId/index.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
-import { Button, Row } from 'react-bootstrap';
-import { LoaderFunction, redirect, useLoaderData } from 'remix';
+import { Alert, Button, Form, Row } from 'react-bootstrap';
+import {
+  ActionFunction,
+  LoaderFunction,
+  redirect,
+  useActionData,
+  useLoaderData,
+  useParams,
+} from 'remix';
 
-import { getCategories } from '~/api/bookCategory';
+import { getCategories, updateBookCategory } from '~/api/bookCategory';
 import { getSession } from '~/sessions';
 import { BookCategory } from '~/types/bookCategory';
 
@@ -26,19 +33,64 @@ export const loader: LoaderFunction = async ({ request, params }) => {
   return data.filter(({ wasPicked }) => !wasPicked);
 };
 
+export const action: ActionFunction = async ({ request, params }) => {
+  const session = await getSession(request.headers.get('Cookie'));
+  if (!session.has('token')) {
+    return redirect('/login');
+  }
+  const { bookGroupId } = params;
+  if (!bookGroupId) {
+    throw new Response('Id missing', {
+      status: 404,
+    });
+  }
+  const token = session.data.token;
+
+  const form = await request.formData();
+
+  const categoryId = form.get('categoryId') as string;
+
+  const data = await updateBookCategory({
+    bookGroupId,
+    token,
+    bookCategoryId: categoryId,
+    body: { isActive: true },
+  });
+
+  return data;
+};
+
 export default function Index() {
-  const [index, setIndex] = useState(-1);
+  const actionData = useActionData<BookCategory>();
+  const { bookGroupId } = useParams();
+
   const bookCategories = useLoaderData<BookCategory[]>();
+  const [index, setIndex] = useState(
+    () => bookCategories.findIndex(({ id }) => id === actionData?.id) || -1
+  );
+  const activeCategory = bookCategories.find(({ isActive }) => isActive);
 
   const getRandom = () => {
     const randomIndex = Math.floor(Math.random() * bookCategories.length);
-
     setIndex(randomIndex);
   };
+
   return (
     <Row>
-      {index !== -1 && bookCategories[index].name}
-      <Button onClick={getRandom}>Wylosuj</Button>
+      {!actionData && activeCategory && (
+        <Alert variant="info">
+          {`Istnieje aktywna kategoria ${activeCategory?.name}. Przed wylosowaniem kolejnej skończ ją omawiać!`}
+        </Alert>
+      )}
+      {index !== -1 && (
+        <Alert variant="success">{`Wylosowano: ${bookCategories[index]?.name}`}</Alert>
+      )}
+      <Form method="POST" action={`/book-group/${bookGroupId}/?index`}>
+        <Form.Control type="hidden" name="categoryId" value={bookCategories?.[index]?.id} />
+        <Button onClick={getRandom} type="submit" disabled={Boolean(activeCategory)}>
+          Wylosuj
+        </Button>
+      </Form>
     </Row>
   );
 }
